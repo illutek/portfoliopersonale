866b87e7613b5fa34e4026e04fd1f9ebc16f113f
Vandenborne Stefan <stefan@illutek.be>
2017-07-11 21:32:46 +0200

    js menu show add

---
+++ js/menu.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Stefan on 11/07/2017.
+ */
+
+    $(document).ready(function () {
+        $('.menu').removeClass('menu-visible');
+
+        $('.menu-show-btn').click(function(){
+            $('.menu').addClass('menu-visible');
+            $('.menu-show-btn').addClass('hidden');
+        });
+
+        $('.close-menu-btn').click(function(){
+            $('.menu').removeClass('menu-visible');
+            $('.menu-show-btn').removeClass('hidden');
+        });
+
+    });
+
